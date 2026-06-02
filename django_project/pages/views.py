93524b0a8d15103ea641c4ca2566a85b0e5e7fd4
chrisbabu9 <chrisbabu9@gmail.com>
2026-06-03 04:30:38 +0530

    Update - 03/06/2026

--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home_page_view(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Home Page")
+
+def about_page_view(request):
+    return render(request,"pages/about.html")
