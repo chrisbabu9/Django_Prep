881581602b1b87dfa5b64d67824832739aaa4ccd
chrisbabu9 <chrisbabu9@gmail.com>
2026-06-03 04:42:29 +0530

    deleted

--- django_project/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def home_page_view(request):
-    return HttpResponse("Home Page")
-
-def about_page_view(request):
-    return render(request,"pages/about.html")
